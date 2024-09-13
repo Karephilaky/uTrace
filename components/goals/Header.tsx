8c77058e9ab10d94a1c1c01f1c83632dbd7adb76
Karephilaky <johannescv417@gmail.com>
2024-09-12 22:55:10 -0500

    icons

--- components/goals/Header.tsx
+++ components/goals/Header.tsx
@@ -1,7 +1,8 @@
-import React from 'react';//johannes
+import React from 'react';
 import { View, TouchableOpacity } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { styles } from '@/styles/goalsStyles';
+import Ionicons from '@expo/vector-icons/Ionicons'; // Importa Ionicons
 
 interface HeaderProps {
   onAddPress: () => void;
@@ -14,10 +15,12 @@ const Header: React.FC<HeaderProps> = ({ onAddPress, onDeletePress }) => {
       <ThemedText style={styles.title}>Objetivos</ThemedText>
       <View style={styles.headerButtons}>
         <TouchableOpacity onPress={onAddPress} style={styles.addButton}>
-          <ThemedText style={styles.addButtonText}>+</ThemedText>
+          {/* Reemplaza el texto "+" con el ícono "add-outline" */}
+          <Ionicons name="add-outline" size={24} color="#fff" />
         </TouchableOpacity>
         <TouchableOpacity onPress={onDeletePress} style={styles.deleteButton}>
-          <ThemedText style={styles.addButtonText}>🗑</ThemedText>
+          {/* Reemplaza el texto "🗑" con el ícono "trash-outline" */}
+          <Ionicons name="trash-outline" size={24} color="#fff" />
         </TouchableOpacity>
       </View>
     </View>
@@ -25,3 +28,4 @@ const Header: React.FC<HeaderProps> = ({ onAddPress, onDeletePress }) => {
 };
 
 export default Header;
+
